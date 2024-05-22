fc2e218e6de83ce80f71b76f9b6d5161e6577fc6
EMMANUEL AYOBAMI <135729444+SCCSMARTCODE@users.noreply.github.com>
2024-05-22 13:46:32 +0100

    Update printf.h

--- printf.h
+++ printf.h
@@ -10,12 +10,16 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <limits.h>
 
 
 /*Defined functions*/
 int _printf(char *str, ...);
 int print_int(int num);
 int print_unsigned(unsigned int num);
+char *print_bin(int num);
+char *decimal_to(int num, int base);
+char *cap_hex_str(char *hex_str);
 
 
 
